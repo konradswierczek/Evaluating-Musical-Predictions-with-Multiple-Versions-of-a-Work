df820e40062ec5c1abe58f258b015795c2d7c4dd
konradswierczek <swierckj@mcmaster.ca>
2025-07-25 20:35:04 +0000

    chore(data): Update to reformatted data only.

--- src/read_data.R
+++ src/read_data.R
@@ -2,49 +2,16 @@
 # Read in and prepare all data for further analysis.
 # ============================================================================ #
 library(readr)
-library(dplyr)
 library(intMAPLEemo)
-library(purrr)
+library(dplyr)
 library(tidyr)
 
 # ============================================================================ #
-# Read data from multiple files and consolidate into one long tibble.
-df_raw <- list.files(
-  "data",
-  pattern = "^_.*\\.csv$",
-  full.names = TRUE
-) |>
-  map_dfr(
-    ~ read_csv(.x) |>
-      select(-1) |>
-      pivot_longer(
-        cols = -filename,
-        names_to = "feature",
-        values_to = "val"
-      )
-  ) |>
-  mutate(
-    setCode = sapply(
-      filename,
-      function(x) helper_translate_filename(x)$setCode
-    ),
-    pieceID = sapply(
-      filename,
-      function(x) helper_translate_filename(x)$pieceID
-    ),
-    pieceID = gsub("-", "_", pieceID),
-    albumID = sapply(
-      filename,
-      function(x) helper_translate_filename(x)$albumID
-    )
-  )
-
-# ============================================================================ #
-# load in data from MIDI analysis.
-df_midi <- read_csv("data/midi.csv")
-
-# ============================================================================ #
-# Load in Essentia Music Extractor Data.
+# Read data.
+df_raw <- read_csv("data/main_data.csv")
+df_midi <- read_csv("data/midi_data.csv")
+df_metadata_albums <- read_csv("data/metadata_albums.csv")
+df_metadata_pieces <- read_csv("data/metadata_pieces.csv")
 df_eme <- read_csv("data/essentiaMusicExtractor.csv") |>
   select(
     "metadata.tags.file_name",
@@ -63,15 +30,6 @@ df_eme <- read_csv("data/essentiaMusicExtractor.csv") |>
     albumID = sapply(
       metadata.tags.file_name,
       function(x) helper_translate_filename(x)$albumID
-    )
-  ) |>
-  filter(
-    setCode == "bach-1",
-    !albumID %in% c(
-      "flatMIDI",
-      "bachAshkenazy2006Deadpan",
-      "bachDemaria2014Deadpan",
-      "bachNewman1973"
     )
   ) |>
   pivot_longer(
