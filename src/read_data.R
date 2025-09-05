dc98348ed05e865ed14528060ed2f1011bdec87c
konradswierczek <swierckj@mcmaster.ca>
2025-09-05 20:46:03 +0000

    feat(data): Remove old eme formatting.

--- src/read_data.R
+++ src/read_data.R
@@ -2,7 +2,6 @@
 # Read in and prepare all data for further analysis.
 # ============================================================================ #
 library(readr)
-library(intMAPLEemo)
 library(dplyr)
 library(tidyr)
 library(stringr)
@@ -14,33 +13,7 @@ df_raw <- read_csv("data/main_data.csv")
 df_midi <- read_csv("data/midi_data.csv")
 df_metadata_albums <- read_csv("data/metadata_albums.csv")
 df_metadata_pieces <- read_csv("data/metadata_pieces.csv")
-df_eme <- read_csv("data/essentiaMusicExtractor.csv") |>
-  select(
-    "metadata.tags.file_name",
-    where(is.double)
-  ) |>
-  mutate(
-    setCode = sapply(
-      metadata.tags.file_name, 
-      function(x) helper_translate_filename(x)$setCode
-    ),
-    pieceID = sapply(
-      metadata.tags.file_name,
-      function(x) helper_translate_filename(x)$pieceID
-    ),
-    pieceID = gsub("-", "_", pieceID),
-    albumID = sapply(
-      metadata.tags.file_name,
-      function(x) helper_translate_filename(x)$albumID
-    )
-  ) |>
-  pivot_longer(
-    cols = -c("setCode", "pieceID", "albumID", "metadata.tags.file_name"),
-    names_to = "feature",
-    values_to = "val"
-  ) |>
-  select(-metadata.tags.file_name)
-write_csv(df_eme, "data/eme_data.csv")
+df_eme <- read_csv("data/eme_data.csv")
 
 # ============================================================================ #
 df_tidy <-  df_raw |>
