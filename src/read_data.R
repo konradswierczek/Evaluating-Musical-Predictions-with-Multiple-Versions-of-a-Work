1646511308985834cebb5f898aca7ba1a4079756
konradswierczek <swierckj@mcmaster.ca>
2025-09-05 20:45:01 +0000

    feat(data): Add eme data.

--- src/read_data.R
+++ src/read_data.R
@@ -5,6 +5,8 @@ library(readr)
 library(intMAPLEemo)
 library(dplyr)
 library(tidyr)
+library(stringr)
+library(forcats)
 
 # ============================================================================ #
 # Read data.
@@ -36,6 +38,52 @@ df_eme <- read_csv("data/essentiaMusicExtractor.csv") |>
     cols = -c("setCode", "pieceID", "albumID", "metadata.tags.file_name"),
     names_to = "feature",
     values_to = "val"
-  )
+  ) |>
+  select(-metadata.tags.file_name)
+write_csv(df_eme, "data/eme_data.csv")
 
 # ============================================================================ #
+df_tidy <-  df_raw |>
+  filter(
+    extractor %in% c(
+      "essentia_tempo_percival", "mirtoolbox_tempo_metre", "librosa_tempo_org",
+      "essentia_onsets_rms", "mirtoolbox_onsets_org", "librosa_onsets_org",
+      "essentia_mode_cqtGomezMIRtoolbox", "mirtoolbox_mode_stftGomezMIRtoolbox", "librosa_mode_censGomezMIRtoolbox",
+      "essentia_centroid_org", "mirtoolbox_centroid_org", "librosa_centroid_org"
+    ),
+    !albumID %in% c(
+      "flatMIDI",
+      "bachAshkenazy2006Deadpan",
+      "bachDemaria2014Deadpan",
+      "bachNewman1973"
+    )
+  ) |>
+  separate_wider_delim(
+    extractor,
+    delim = "_",
+    names = c(
+      "tool",
+      "feature",
+      "method"
+    )
+  ) |>
+  mutate(
+    tool = case_when(
+      tool == "mirtoolbox" ~ "MIRtoolbox",
+      .default = str_to_title(tool)
+    ),
+    feature = case_when(
+      feature == "tempo" ~ "Tempo (BPM)",
+      feature == "mode" ~ "Relative Mode",
+      feature == "centroid" ~ "Spectral Centroid (Hz)",
+      feature == "onsets" ~ "Onsets (#)",
+    ),
+    feature = as.factor(feature),
+    feature = fct_relevel(
+      feature,
+      'Relative Mode',
+      "Onsets (#)",
+      'Tempo (BPM)',
+      'Spectral Centroid (Hz)'
+    )
+  )
